ecabba91ecf67630c22c7f328eaa33f019a5504d
Saathvik.Katkam <Saathvik.Katkam@192.168.0.4>
2017-07-16 00:40:44 +0530

    2nd time - release

--- src/main/java/TestMerge.java
+++ src/main/java/TestMerge.java
@@ -4,6 +4,7 @@ public class TestMerge {
 	void testMerge() {
 		System.out.println("master - merge1 & merge 2");
 		System.out.println("added from feature branch and relese branch");
+		System.out.println("2nd time - release branch");
 		
 	}
 }
