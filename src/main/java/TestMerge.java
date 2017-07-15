2e5403ab8caacc3dcdb45757bac875d9322f4a02
Saathvik.Katkam <Saathvik.Katkam@192.168.0.4>
2017-07-16 00:43:05 +0530

    2nd time - feature

--- src/main/java/TestMerge.java
+++ src/main/java/TestMerge.java
@@ -4,6 +4,7 @@ public class TestMerge {
 	void testMerge() {
 		System.out.println("master - merge1 & merge 2");
 		System.out.println("added from feature branch");
+		System.out.println("2nd time - feature");
 		
 	}
 }
